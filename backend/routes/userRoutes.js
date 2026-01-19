0b1e968e9e90fa1188cfe567ad13e6baaa661d3d
iamsaba786 <snaz7571@gmail.com>
2026-01-19 19:56:24 +0530

    ss

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,14 +14,12 @@ import { updateUserProfile } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
-const upload = multer({ storage }).single("avatar");
-
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", isAuth, logOutUser);
 router.get("/me", isAuth, myProfile);
 router.get("/:id", isAuth, userProfile);
 router.post("/follow/:id", isAuth, followAndUnfollowUser);
-router.put("/update", isAuth, upload, uploadProfilePic, updateUserProfile);
+router.put("/update", isAuth, uploadProfilePic, updateUserProfile);
 
 export default router;
