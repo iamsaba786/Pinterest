707599365686ee082e2fb177f5459bf46a8734a1
iamsaba786 <snaz7571@gmail.com>
2026-01-19 19:17:57 +0530

    api name mismatch

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 cloudinary.v2.config({
   cloud_name: process.env.Cloud_Name,
-  api_key: process.env.Cloud_Api,
+  api_key: process.env.Cloud_API,
   api_secret: process.env.Cloud_Secret,
 });
 
