50fd3c04570b0ca29b86145282c5ec6661e9e773
iamsaba786 <snaz7571@gmail.com>
2026-02-01 13:27:27 +0530

    expires time extend

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,11 +2,11 @@ import jwt from "jsonwebtoken";
 
 const generateToken = (id, res) => {
   const token = jwt.sign({ id }, process.env.JWT_SEC, {
-    expiresIn: "15d",
+    expiresIn: "1y",
   });
 
   res.cookie("token", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    maxAge: 365 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
     sameSite: "None",
